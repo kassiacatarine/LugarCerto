8f16b0bd6ce13c3d9afc9e3ca809ed7a433028f6
Kássia Catarine <kassia_catarine.15@hotmail.com>
2020-05-09 23:57:55 -0300

    Alterações de declarações de variáveis

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
-var xmlhttp = new XMLHttpRequest();
-var url = 'https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72';
+const xmlhttp = new XMLHttpRequest();
+const url = 'https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72';
 
-xmlhttp.onreadystatechange = function() {
+xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
-    var myArr = JSON.parse(this.responseText);
-    console.log(myArr);
-    
+    let myArr = JSON.parse(this.responseText);
     housesCarousel(myArr);
   }
 };
@@ -13,14 +11,23 @@ xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
 function housesCarousel(arr) {
-    let out = "";
-    for(let i = 0; i < arr.length - 19; i++) {
-      out += `<a href="/details.html" class="item">
-                <img src="${arr[i].photo}" alt="${arr[i].name}" >
-                <p class="type">${arr[i].property_type}</p>
-                <p class="name">${arr[i].name}</p>
-                <p class="price">R$ ${arr[i].price}</p>
-              </a>`;
-    }
-    document.getElementById("houses").innerHTML = out;
+  let out = "";
+  for (let i = 0; i < arr.length - 21; i++) {
+    out += `<div class="card" style="width: 15rem;">
+              <img src="${arr[i].photo}" alt="${arr[i].name}" class="card-img-top img-card-offers">
+              <div class="card-header">
+                <h5>${arr[i].name}</h5>
+                <p>${arr[i].property_type}</p>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">R$ ${arr[i].price}</h5>
+                <p class="card-text"></p>
+                <p class="card-text"></p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </div>
+            </div>`;
+  }
+  document.getElementById("ofertas").innerHTML = out;
 }
